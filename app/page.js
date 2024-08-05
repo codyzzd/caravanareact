3ddc62cb2ca97dcac0e492fe202755ae59a04190
codyzzd <codyzzd@gmail.com>
2024-08-05 16:21:33 -0300

    test flowbite

--- app/page.js
+++ app/page.js
@@ -1,3 +1,15 @@
+import { Dropdown } from "flowbite-react";
+
 export default function Home() {
-  return <>asdsda</>;
+  return (
+    <>
+      asdsda
+      <Dropdown label="Dropdown button">
+        <Dropdown.Item>Dashboard</Dropdown.Item>
+        <Dropdown.Item>Settings</Dropdown.Item>
+        <Dropdown.Item>Earnings</Dropdown.Item>
+        <Dropdown.Item>Sign out</Dropdown.Item>
+      </Dropdown>
+    </>
+  );
 }
